14450e0a5e66d420575cd75e3469e31314b3edac
John-Gallardo <johnpatrickgallardo33@gmail.com>
2026-08-20 23:27:14 -0600

    feat: add code to process escape key

--- src/Input/Input.cpp
+++ src/Input/Input.cpp
@@ -1,1 +1,8 @@
 #include "Input.h"
+#include "Window/Window.h"
+
+void Input::processUserInput(Window &window) const {
+    if (window.isKeyPressed(GLFW_KEY_ESCAPE)) {
+        window.closeWindow();
+    }
+}
--- src/Input/Input.h
+++ src/Input/Input.h
@@ -1,8 +1,9 @@
 #pragma once
+#include "Window/Window.h"
 
 class Input {
 public:
-
+    void processUserInput(Window &window) const;
 private:
-    
+
 };
