ccdca4e0c949acc2b686a0dbd6dda50d15c6767a
John-Gallardo <johnpatrickgallardo33@gmail.com>
2026-08-20 23:26:27 -0600

    feat & refactor: add maxFramesInFlight for later & change title to const char * to be simpler (GLFW uses it)

--- src/Config.h
+++ src/Config.h
@@ -1,7 +1,8 @@
 #pragma once
 
 namespace Config{
-    inline constexpr int         width {1000};
-    inline constexpr int         height{1000};
-    inline constexpr std::string title {"Vulkan Engine"};
+    inline constexpr int         width            {1000};
+    inline constexpr int         height           {1000};
+    inline constexpr const char *title            {"Vulkan Engine"};
+    inline constexpr int         maxFramesInFlight{2};
 }
