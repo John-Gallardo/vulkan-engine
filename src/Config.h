39d1ec10adfc8667bbdce55f2f17212ec2e921c3
John-Gallardo <johnpatrickgallardo33@gmail.com>
2026-08-20 22:15:49 -0600

    feat: add config file. contains window width, height, & title

---
+++ src/Config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Config{
+    inline constexpr int         width {1000};
+    inline constexpr int         height{1000};
+    inline constexpr std::string title {"Vulkan Engine"};
+}
