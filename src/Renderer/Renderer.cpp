a3849be203e89d78b57a1b905e4cf254ad05a81e
John-Gallardo <johnpatrickgallardo33@gmail.com>
2026-08-21 18:23:17 -0600

    feat: finished pickPhysicalDevice

--- src/Renderer/Renderer.cpp
+++ src/Renderer/Renderer.cpp
@@ -2,40 +2,96 @@
 #include "Config.h"
 #include "Window/Window.h"
 #include <volk.h>
+#include <ranges>
+#include <utility>
 #include <stdexcept>
+#include <string_view>
+#include <string>
+#include <cstdint>
+#include <vector>
+#include <queue>
+#include <print>
 
 void Renderer::initVulkan(Window &window) {
-    if (volkInitialize() != VK_SUCCESS) {
-        throw std::runtime_error("Error: failed to initialize Volk");
-    }
+    checkResult(volkInitialize(), "Error: failed to initialize Volk");
     createInstance(window);
-
+    pickPhysicalDevice();
+    createDevice();
 }
 
 void Renderer::createInstance(Window &window) {
     constexpr VkApplicationInfo applicationInfo{
-        .sType             {VK_STRUCTURE_TYPE_APPLICATION_INFO},
-        .pNext             {nullptr},
-        .pApplicationName  {Config::title},
-        .applicationVersion{Config::version},
-        .pEngineName       {nullptr},
-        .engineVersion     {},
-        .apiVersion        {VK_API_VERSION_1_4}
+        .sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        .pNext              = nullptr,
+        .pApplicationName   = Config::title,
+        .applicationVersion = Config::version,
+        .pEngineName        = nullptr,
+        .engineVersion      = {},
+        .apiVersion         = VK_API_VERSION_1_4,
     };
 
     // grab requried instance extensions for GLFW
     auto [glfwExtensionCount, glfwExtensions] = window.getRequiredInstanceExtensions();
 
     const VkInstanceCreateInfo instanceCreateInfo{
-        .sType                  {VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO},
-        .pNext                  {nullptr},
-        .flags                  {},
-        .pApplicationInfo       {&applicationInfo},
-        .enabledLayerCount      {0},
-        .ppEnabledLayerNames    {nullptr},
-        .enabledExtensionCount  {glfwExtensionCount},
-        .ppEnabledExtensionNames{glfwExtensions}
+        .sType                   = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pNext                   = nullptr,
+        .flags                   = {},
+        .pApplicationInfo        = &applicationInfo,
+        .enabledLayerCount       = 0,
+        .ppEnabledLayerNames     = nullptr,
+        .enabledExtensionCount   = glfwExtensionCount,
+        .ppEnabledExtensionNames = glfwExtensions
     };
 
-    vkCreateInstance(&instanceCreateInfo, nullptr, &m_instance);
+    checkResult(vkCreateInstance(&instanceCreateInfo, nullptr, &m_instance), "Error: failed to create Vulkan instance");
+    volkLoadInstance(m_instance);
+}
+
+void Renderer::pickPhysicalDevice() {
+    // enumerate physical devices
+    uint32_t physicalDeviceCount{};
+    checkResult(vkEnumeratePhysicalDevices(m_instance, &physicalDeviceCount, nullptr), "Error: failed to enumerate physical device count");
+    std::vector<VkPhysicalDevice> physicalDevices(physicalDeviceCount);
+    checkResult(vkEnumeratePhysicalDevices(m_instance, &physicalDeviceCount, physicalDevices.data()), "Error: failed to fill physicalDevices vector");
+
+    // score based on if it's a dGPU or iGPU. we pick dGPU first
+    std::priority_queue<std::pair<int, VkPhysicalDevice>> priorityQueue{};
+    for (VkPhysicalDevice physicalDevice : physicalDevices) {
+        VkPhysicalDeviceProperties deviceProperties{};
+        vkGetPhysicalDeviceProperties(physicalDevice, &deviceProperties);
+        
+        int score{};
+        switch (deviceProperties.deviceType) {
+            case VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU:
+                score += 2;
+                break;
+            case VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU:
+                score += 1;
+                break;
+            default:
+                continue;
+        }
+        priorityQueue.push({score, physicalDevice});
+    }
+
+    if (!priorityQueue.empty()) {
+        m_physicalDevice = priorityQueue.top().second;
+    } else {
+        throw std::runtime_error("Error: failed to find compatible GPU");
+    }
+
+    VkPhysicalDeviceProperties deviceProperties{};
+    vkGetPhysicalDeviceProperties(m_physicalDevice, &deviceProperties);
+    std::println("Selected GPU: {}", deviceProperties.deviceName);
+}
+
+void Renderer::createDevice() {
+
+}
+
+void Renderer::checkResult(VkResult result, std::string_view errorMessage) const {
+    if (result != VK_SUCCESS) {
+        throw std::runtime_error(std::string(errorMessage));
+    }
 }
--- src/Renderer/Renderer.h
+++ src/Renderer/Renderer.h
@@ -1,12 +1,19 @@
 #pragma once
 #include <volk.h>
+#include <string_view>
 class Window;
 
 class Renderer {
 public:
     void initVulkan(Window &window);
 private:
-    VkInstance m_instance{VK_NULL_HANDLE};
+    VkInstance       m_instance      {VK_NULL_HANDLE};
+    VkPhysicalDevice m_physicalDevice{VK_NULL_HANDLE};
+    VkDevice         m_device        {VK_NULL_HANDLE};
 
     void createInstance(Window &window);
+    void pickPhysicalDevice();
+    void createDevice();
+    void checkResult(VkResult result, std::string_view errorMessage) const;
+    void cleanup();
 };
